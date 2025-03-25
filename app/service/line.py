6629f1e02b5eb22fdf743c7c3aa4607ed5932a81
carbarcha <samhoward13261@gmail.com>
2025-03-25 18:39:46 +0800

    :sparkles: update hao report text

--- app/service/line.py
+++ app/service/line.py
@@ -4,6 +4,8 @@ from app.db.report import get_today_report
 from app.service.report import create_major_investors_report, create_futures_report, create_margin_report
 from app.service.youtube import get_today_hao_report
 
+from datetime import datetime
+
 def daily_report(event_id:str, report_type:str, data_number=20):
     """Send daily report to event"""
     # TODO 時間檢查
@@ -24,11 +26,14 @@ def daily_report(event_id:str, report_type:str, data_number=20):
     push_message(to=event_id, message=result.msg, img_url=result.url)
     return
 
+hao_report_msg = "【游庭皓的財經皓角】 {date} 報告總結\n\n{summary}"
 def hao_report(event_id:str):
     """Send Hao report to event"""
+    today = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0).strftime('%Y-%m-%d')
     success, data, err_msg = get_today_hao_report()
     if success:
-        push_message(to=event_id, message=data.vid_summary)
+        push_message(to=event_id, message=data.vid_url)
+        push_message(to=event_id, message=hao_report_msg.format(date=today, summary=data.vid_summary))
     else:
         push_message(to=event_id, message=f"財金皓角 daily report 查詢失敗，錯誤訊息: {err_msg}")
     return 
