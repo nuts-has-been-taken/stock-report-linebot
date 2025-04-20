4b26f7d0d865b380a206581ee47666dbd42a919a
carbarcha <samhoward13261@gmail.com>
2025-04-20 12:30:34 +0800

    :construction: extract common function

--- app/service/youtube.py
+++ app/service/youtube.py
@@ -7,6 +7,31 @@ from datetime import date, timedelta
 
 AUDIO_MODE = openai_client.AUDIO_MODE
 
+def process_youtube_data(channel_id, current_date, url=None):
+    if not url:
+        title, url, vid_date = get_live_stream(channel_id, current_date)
+    else:
+        title, vid_date = None, current_date  # 如果 URL 已提供，則不需要再取得
+
+    if url:
+        # 下載 youtube 字幕
+        subtitle = get_youtube_subtitles(url)
+        if not subtitle and not AUDIO_MODE:
+            return None, "字幕未上傳"
+        elif not subtitle and AUDIO_MODE:
+            audio = get_youtube_audio(url)
+            print("使用音訊進行 summary")
+            summary = create_summary_audio(audio)
+        else:
+            # 呼叫 gpt 生成報告
+            summary = create_summary(subtitle)
+        img_url = get_youtube_img(url) if title else None
+        # 儲存報告
+        data = save_youtube_vid('游庭皓的財經皓角', channel_id, vid_date, title, url, summary, img_url)
+        return data, None
+    else:
+        return None, "無法取得影片"
+
 # 游庭皓的財經皓角
 def get_today_hao_report():
     # 從資料庫取得資料
@@ -16,24 +41,10 @@ def get_today_hao_report():
         title, url, vid_date = get_latest_live_stream(channel_id)
         if vid_date != date.today():
             return False, None, "今日無直播"
-        if url:
-            # 下載 youtube 字幕
-            subtitle = get_youtube_subtitles(url)
-            if not subtitle and not AUDIO_MODE:
-                return False, None, "今日字幕還沒上傳，請稍後在試"
-            elif not subtitle and AUDIO_MODE:
-                audio = get_youtube_audio(url)
-                print("使用音訊進行 summary")
-                summary = create_summary_audio(audio)
-            else:
-                # 呼叫 gpt 生成報告
-                summary = create_summary(subtitle)
-            img_url = get_youtube_img(url)
-            # 儲存報告
-            data = save_youtube_vid('游庭皓的財經皓角', channel_id, vid_date, title, url, summary, img_url)
-            return True, data, None
-        else:
-            return False, None, "取得影片失敗"
+        data, error = process_youtube_data(channel_id, date.today(), url)
+        if error:
+            return False, None, error
+        return True, data, None
     else:
         return True, data, None
 
@@ -44,32 +55,14 @@ def get_hao_report(start_date:date, end_date:date):
     all_data = []
 
     while current_date <= end_date:
-        # 從資料庫取得當天資料
         data = get_youtube_vid(channel_id, current_date)
         if not data:
-            title, url, vid_date = get_live_stream(channel_id, current_date)
-            if url:
-                # 下載 youtube 字幕
-                subtitle = get_youtube_subtitles(url)
-                if not subtitle and not AUDIO_MODE:
-                    current_date += timedelta(days=1)
-                    continue
-                elif not subtitle and AUDIO_MODE:
-                    audio = get_youtube_audio(url)
-                    print("使用音訊進行 summary")
-                    summary = create_summary_audio(audio)
-                else:
-                    # 呼叫 gpt 生成報告
-                    summary = create_summary(subtitle)
-                # 儲存報告
-                data = save_youtube_vid('游庭皓的財經皓角', channel_id, vid_date, title, url, summary)
-                all_data.append(data)
-            else:
+            data, error = process_youtube_data(channel_id, current_date)
+            if error:
                 current_date += timedelta(days=1)
                 continue
-        else:
+        if data:
             all_data.append(data)
-        
         current_date += timedelta(days=1)
 
     if not all_data:
